ce5a6c0c4c82b67609382cec4f560e31c3d88e44
Daniel_M_Williams <equipoise@gmail.com>
2019-11-05 17:01:24 -0500

    [xp][5] Implemented binary tree array-implementation

--- _5_CPython_CAPI/bst/tree.c
+++ _5_CPython_CAPI/bst/tree.c
@@ -1,16 +1,106 @@
 #include <stdlib.h>
+#include <string.h>
 
 #include "Python.h"
 
 #include "node.h"
 
+// Binary trees can also be stored in breadth-first order as an implicit data structure in arrays:
+//   - If a node has an index i, its children are found at indices `2i+1` (for the left child) and `2i+2` (for the right)
+//   - If a node has an index i, its parent (if any) is found at index `floor((i-1)/(2))`  (assuming the root has index zero). 
+// This method benefits from more compact storage and better locality of reference, particularly during a preorder traversal. 
+// However, it is expensive to grow and wastes space proportional to 2h - n for a tree of depth h with n nodes.
+// 
+// +---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+
+// | 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10| 11| 12| 13| 14|
+// +---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+
+//   15
+//        7  23
+//                3  11  19  27
+//                                1   5   9  13  17  21  25  29
+//
+// +---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+
+// | 15|  7| 23|  3| 11| 19| 27|  1|  5|  9| 13| 17| 21| 25| 29|
+// +---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+
+//
+// Index  -->  Index 
+//    0 -> l -> 1
+//    0 -> r -> 2
+//    5 -> l -> 11
+//    6 -> r -> 14
+//
+
+
 typedef struct _binary_search_tree {
     PyObject_HEAD;
-    BinarySearchNode * root;
+    int * data;
+    int allocated;
 } BinarySearchTree;
 
+const int SENTINEL_VALUE = 0;
+
+void tree_add(BinarySearchTree* self, int new_value){
+    // fprintf(stderr, "++ adding value: %2d\n", new_value);
+    int search_index = 0;
+    while(search_index < self->allocated){
+        // fprintf(stderr, "    @[ %2d] = %2d\n", search_index, self->data[search_index] );
+        if( SENTINEL_VALUE == self->data[search_index] ){
+            self->data[search_index] = new_value;
+            return;
+        }else if(self->data[search_index] > new_value){
+            // increment to left branch
+            search_index = 2 * search_index + 1;
+        }else{
+            // increment to right branch
+            search_index = 2 * search_index + 2;
+        }
+    }
+}
+
+void tree_dump(BinarySearchTree* self, FILE* dest ){
+    fprintf(dest, "-- [");
+    for( int i=0; i < self->allocated; ++i ){
+        fprintf(dest, " %2d", i);
+    }
+    fprintf(dest, " ] -- \n");
+    
+    fprintf(dest, "## [");
+    for( int i=0; i < self->allocated; ++i ){
+        if( self->data[i] < 0 || 0xFF < self->data[i]){
+            fprintf(dest, " --");
+        }else{
+            fprintf(dest, " %2d", self->data[i]);
+        }
+    }
+    fprintf(dest, " ]\n");
+}
+
+void tree_reset(BinarySearchTree* self, int size){
+    if(self->allocated >= size){
+        return;
+    }
+
+    int next_size = 4;
+    while( next_size < size){
+        next_size <<= 1;
+    }
+
+    int* new_buffer = malloc(sizeof(int)*next_size);
+    memset(new_buffer, SENTINEL_VALUE, sizeof(int)*next_size);
+    if( NULL != self->data){
+        memcpy(new_buffer, self->data, self->allocated);
+        free(self->data);
+    }
+    
+    self->data = new_buffer;
+    self->allocated = next_size;
+
+    return;
+}
+
 static int BinarySearchTree_init(BinarySearchTree *self, PyObject *args){
-    self->root = NULL;
+    self->data = NULL;
+    self->allocated = 0;
 
     PyObject* list = NULL;
     if( ! PyArg_ParseTuple(args, "O", &list)){
@@ -20,16 +110,15 @@ static int BinarySearchTree_init(BinarySearchTree *self, PyObject *args){
         return -1;
     }
 
-    if( PyLong_Check(PyList_GetItem(list, 0)) ){
-        int root_value = PyLong_AsLong( PyList_GetItem(list, 0) );
-        self->root = node_new( root_value );
+    int new_size = PyList_Size(list);
+    if( 0 < new_size ){
+        tree_reset(self, new_size);
     }
 
-    for( int i=1; i < PyList_Size(list); ++i ){
+    for( int i=0; i < PyList_Size(list); ++i ){
         int each_value = PyLong_AsLong( PyList_GetItem(list, i) );
-        node_add( self->root, each_value );
+        tree_add( self, each_value );
     }
-
 
     return 0;
 }
@@ -37,24 +126,32 @@ static int BinarySearchTree_init(BinarySearchTree *self, PyObject *args){
 static void BinarySearchTree_dealloc(BinarySearchTree *self){
     PyTypeObject *tp = Py_TYPE(self);
 
-    // free references and buffers here
-    node_free(self->root);
+    self->allocated = 0;
+    free(self->data);
 
     tp->tp_free(self);
     Py_DECREF(tp);
 }
 
-static PyObject* tree_search( PyObject* self, PyObject* search_value){
-    // ignore null check
-
-    BinarySearchTree* tree = (BinarySearchTree*) self;
-    if( ! PyLong_Check(search_value)){
+static PyObject* tree_search( PyObject* self, PyObject* search_object){
+    if( ! PyLong_Check(search_object)){
         Py_RETURN_NONE;
     }
+    int search_value = PyLong_AsLong(search_object);
 
-    int found_value = node_search( tree->root, PyLong_AsLong(search_value));
-    if( 0 < found_value ){
-        return Py_BuildValue("i", found_value);
+    BinarySearchTree* tree = (BinarySearchTree*)self;
+    int search_index = 0;
+    while(search_index < tree->allocated){
+        if( search_value == tree->data[search_index] ){
+            tree->data[search_index] = search_value;
+            return Py_BuildValue("i", tree->data[search_index]);
+        }else if(tree->data[search_index] > search_value){
+            // increment to left branch
+            search_index = 2 * search_index + 1;
+        }else{
+            // increment to right branch
+            search_index = 2 * search_index + 2;
+        }
     }
 
     Py_RETURN_NONE;
